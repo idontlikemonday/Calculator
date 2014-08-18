585c8ecf7b750ece2cd1d7d3565f18cbdd054d2d
idontlikemonday <--->
2014-08-18 20:44:00 +0700

    Initial commit. Base functional of calculator. Added one operation - addition.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    // TODO: Make it singleton
+    public class Calculator
+    {
+        private Operand _first = new Operand();
+        private Operand _second = new Operand();
+        private double _result;
+
+        private delegate double OperatorDelegate(double x, double y);
+        private Dictionary<string, OperatorDelegate> _operations;
+
+        public string Operator { get; set; }
+
+        private double _firstOp
+        {
+            get
+            {
+                return _first.Value;
+            }
+            set
+            {
+                _first.Value = value;
+            }
+        }
+        private double _secondOp
+        {
+            get
+            {
+                return _second.Value;
+            }
+            set
+            {
+                _second.Value = value;
+            }
+        }
+        public double Result
+        {
+            get
+            {
+                if (!Double.IsNaN(_result))
+                {
+                    return _result;
+                }
+                else
+                {
+                    if (!Double.IsNaN(CurrentOperand.Value))
+                    {
+                        return CurrentOperand.Value;
+                    }
+                    return 0d;
+                }
+            }
+        }
+
+        private Operand CurrentOperand
+        {
+            get
+            {
+                return (IsFirstOpReady ? _second : _first);
+            }
+        }
+
+        public bool IsFirstOpReady
+        {
+            get
+            {
+                return !String.IsNullOrEmpty(Operator);
+            }
+        }
+
+
+        public Calculator()
+        {
+            _result = Double.NaN;
+
+            _operations =
+            new Dictionary<string, OperatorDelegate>
+            {
+                { "+", delegate(double a, double b) {return a + b;} },
+               // { "-", this.DoSubtraction },
+               // { "*", this.DoMultiplication },
+               // { "/", this.DoDivision },
+            };
+        }
+
+        public void AddDigitToCurrentOperand(byte digit)
+        {
+            if (Double.IsNaN(CurrentOperand.Value))
+            {
+                CurrentOperand.Value = digit;
+            }
+            else
+            {
+                CurrentOperand.Value = CurrentOperand.Value * 10 + digit;
+            }
+        }
+
+        public void AddOperator(string operatorToAdd)
+        {
+            Operator = operatorToAdd;
+            _secondOp = Double.NaN;
+        }
+
+        public List<string> GetAvailableOperators()
+        {
+            return _operations.Keys.ToList();
+        }
+
+        public bool IsAvailableOperator(string operatorToCheck)
+        {
+            return GetAvailableOperators().Contains(operatorToCheck);
+        }
+
+        public void ExecuteOperation()
+        {
+            if (Double.IsNaN(_firstOp))
+            {
+                throw new Exception("First Operand Is Missing");
+            }
+            if (Double.IsNaN(_secondOp))
+            {
+                throw new Exception("Second Operand Is Missing");
+            }
+            if (String.IsNullOrEmpty(Operator))
+            {
+                throw new Exception("Operator Is Missing");
+            }
+            if (!IsAvailableOperator(Operator))
+            {
+                throw new Exception("Operator Is Not Available");
+            }
+            _result = _operations[Operator](_firstOp, _secondOp);
+
+            AfterOperation();
+        }
+
+        private void AfterOperation()
+        {
+            _firstOp = _result;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            while (true)
+            {
+                var key = Console.ReadKey();
+
+                if (Char.IsDigit(key.KeyChar))
+                {
+                    calc.AddDigitToCurrentOperand(Convert.ToByte(key.KeyChar.ToString()));
+                }
+                if (calc.IsAvailableOperator(key.KeyChar.ToString()))
+                {
+                    calc.AddOperator(key.KeyChar.ToString());
+                }
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    calc.ExecuteOperation();
+                    Console.WriteLine();
+                    Console.WriteLine(calc.Result);
+                }
+            }
+        }
+    }
+}
